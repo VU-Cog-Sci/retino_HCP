09aeec80705d2dce8606dd8b0dc02505c7062eeb
Tomas Knapen <tknapen@gmail.com>
2018-03-02 18:03:09 +0100

    first actual job submission

--- scripts/submit_prf_jobs.py
+++ scripts/submit_prf_jobs.py
@@ -7,7 +7,7 @@ import json
 import sys
 import nibabel as nb
 
-jobscript_template_file = os.path.join(os.getcwd(), 'prf_jobscript_template.sh')
+jobscript_template_file = os.path.join(os.getcwd(), 'jobscript_template.sh')
 
 with open('../settings.json') as f:
     json_s = f.read()
@@ -18,23 +18,23 @@ base_dir = analysis_info['cluster_base_folder']
 subject_directories = [os.path.split(fn)[1] for fn in glob.glob(os.path.join(base_dir, '*')) if os.path.isdir(fn)]
 
 
-for sd in subject_directories:
-            jobscript = open(jobscript_template_file)
-            working_string = jobscript.read()
-            jobscript.close()
+for sd in subject_directories[:10]:
+    jobscript = open(jobscript_template_file)
+    working_string = jobscript.read()
+    jobscript.close()
 
-            RE_dict = {
-                '---SUBJECT---':                sd
-            }
+    RE_dict = {
+        '---SUBJECT---':                sd
+    }
 
-            for e in RE_dict.keys():
-                working_string = working_string.replace(e, RE_dict[e])
+    for e in RE_dict.keys():
+        working_string = working_string.replace(e, RE_dict[e])
 
-            js_name = os.path.expanduser(os.path.join('~', 'jobs', sd + '.sh'))
-            of = open(js_name, 'w')
-            of.write(working_string)
-            of.close()
+    js_name = os.path.expanduser(os.path.join('~', 'jobs', sd + '.sh'))
+    of = open(js_name, 'w')
+    of.write(working_string)
+    of.close()
 
-            print('submitting ' + js_name + ' to queue')
-            print(working_string)
-            # os.system('sbatch ' + js_name)
+    print('submitting ' + js_name + ' to queue')
+    print(working_string)
+    os.system('sbatch ' + js_name)
