12146a94b1e3af59d9d2f6d47ae1e3397c9c9050
Tomas Knapen <tknapen@gmail.com>
2018-03-24 09:50:44 +0100

    updated to start with gii analysis

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -26,3 +26,30 @@ for sd in subject_directories:
     av_bar_file = average_bar_ciis(pp_out_files[4],pp_out_files[5])
 
 
+
+
+
+#################################################################################
+
+with open('../settings.json') as f:
+    json_s = f.read()
+    analysis_info = json.loads(json_s)
+
+base_dir = analysis_info['cluster_base_folder'] 
+
+subject_directories = [fn for fn in glob.glob(os.path.join(base_dir, '*')) if os.path.isdir(fn)]
+
+for sd in subject_directories:
+    os.chdir(sd)
+    cii_files = glob.glob('*.dseries.nii')
+
+    wbc = """wb_command -cifti-separate {cii} \
+        COLUMN -volume-all data_sub.nii \
+        -metric CORTEX_LEFT {cii_n}_L.func.gii \
+        -metric CORTEX_RIGHT {cii_n}_R.func.gii"""
+
+    for cii in cii_files:
+        wbc_c = wbc.format(cii=cii, cii_n=cii[-4])
+
+        print(wbc_c)
+        # os.system(wbc_c)
