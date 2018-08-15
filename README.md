f2706e8afc270b7055308314cc2c0f6857075f84
Martin Szinte <martin.szinte@gmail.com>
2018-08-15 15:58:12 +0200

    first working version with pycortex maps of prf derivatives running

--- README.md
+++ README.md
@@ -21,5 +21,11 @@ martin_dev
 - Best subjects (based on R2 of V1 voxel selected by glasser2016 attlas) were chosen using data
   issued from Benson et al (http://dx.doi.org/10.1101/308247)
 - pRF parameters are extracted using scripts/submit_prf_jobs.py on Lisa
-- pRF parameters are analysed using pp_roi.py
+- pRF derivatives are analysed using pp_roi.py
+- pRF derivatives data are resampled to fsaverage using pp_roi.py
+- pRF derivatives are plotted with pycortex using pp_roi.py
+- ROI are determined from Conte69 atlas using pp_roi.py
+- pRF derivatives are saved as a function of ROI in hdf5 files using pp_roi.py
+- PRF derivatives summary are plotted with bokeh using post_pp_roi.py
+
 
--- retino_HCP/utils.py
+++ retino_HCP/utils.py
@@ -590,4 +590,169 @@ def mask_gii_2_hdf5(in_file, mask_file, hdf5_file, folder_alias):
 
     return None
 
+def draw_cortex_vertex(subject,data,cmap,vmin,vmax,cbar = 'discrete',cmap_steps = 255,\
+                        alpha = None,depth = 1,thick = 1,height = 1024,sampler = 'nearest',\
+                        with_curvature = True,with_labels = False,with_colorbar = False,\
+                        with_borders = False,curv_brightness = 0.95,curv_contrast = 0.05,add_roi = False,\
+                        roi_name = 'empty',col_offset = 0):
+    """
+    Plot brain data onto a previously saved flatmap.
 
+    Parameters
+    ----------
+    subject             : subject id (e.g. 'sub-001')
+    data                : the data you would like to plot on a flatmap
+    cmap                : colormap that shoudl be used for plotting
+    vmin                : minimal value iqn colormap
+    vmax                : maximal value in colormap
+    cbar                : color bar layout
+    cmap_steps          : number of colormap bins
+    alpha               : alpha map
+    depth               : Value between 0 and 1 for how deep to sample the surface for the flatmap (0 = gray/white matter boundary, 1 = pial surface)
+    thick               : Number of layers through the cortical sheet to sample. Only applies for pixelwise = True
+    height              : Height of the image to render. Automatically scales the width for the aspect of the subject's flatmap
+    sampler             : Name of sampling function used to sample underlying volume data. Options include 'trilinear', 'nearest', 'lanczos'
+    with_curvature      : Display the rois, labels, colorbar, annotated flatmap borders, or cross-hatch dropout?
+    with_labels         : Display labels?
+    with_colorbar       : Display pycortex' colorbar?
+    with_borders        : Display borders?
+    curv_brightness     : Mean brightness of background. 0 = black, 1 = white, intermediate values are corresponding grayscale values.
+    curv_contrast       : Contrast of curvature. 1 = maximal contrast (black/white), 0 = no contrast (solid color for curvature equal to curvature_brightness).
+    add_roi             : add roi -image- to overlay.svg
+    roi_name            : roi name
+    col_offset          : colormap offset between 0 and 1
+
+    Returns
+    -------
+    vertex_rgb - pycortex vertex file
+    """
+    
+    import cortex
+    import pdb
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.colors as colors
+    from matplotlib import cm
+    import matplotlib as mpl
+    import ipdb
+    deb = ipdb.set_trace
+
+    # define colormap
+    base = cortex.utils.get_cmap(cmap)
+    val = np.fmod(np.linspace(0+col_offset, 1+col_offset,cmap_steps+1,endpoint=False),1.0)
+    colmap = colors.LinearSegmentedColormap.from_list(  'my_colmap',
+                                                        base(val),
+                                                        N = cmap_steps)
+
+    # convert data to RGB
+    vrange = float(vmax) - float(vmin)
+    norm_data = ((data-float(vmin))/vrange)*cmap_steps
+    mat = colmap(norm_data.astype(int))
+
+    vertex_rgb = cortex.VertexRGB(  red = mat[...,0],
+                                    green = mat[...,1],
+                                    blue = mat[...,2],
+                                    alpha = alpha,
+                                    subject = subject)
+    
+    vertex_rgb_fig = cortex.quickflat.make_figure(  braindata = vertex_rgb,
+                                                    depth = depth,
+                                                    thick = thick,
+                                                    height = height,
+                                                    sampler = sampler,
+                                                    with_curvature = with_curvature,
+                                                    with_labels = with_labels,
+                                                    with_colorbar = with_colorbar,
+                                                    with_borders = with_borders,
+                                                    curvature_brightness = curv_brightness,
+                                                    curvature_contrast = curv_contrast)
+    
+    # Color bars
+    if cbar == 'polar':
+
+        # Polar angle color bar
+        colorbar_location = [0.5, 0.07, 0.8, 0.2]
+        n = 200
+        cbar_axis = vertex_rgb_fig.add_axes(colorbar_location, projection='polar')
+        norm = mpl.colors.Normalize(0, 2*np.pi)
+
+        # Plot a color mesh on the polar plot
+        # with the color set by the angle
+        t = np.linspace(2*np.pi,0,n)
+        r = np.linspace(1,0,2)
+        rg, tg = np.meshgrid(r,t)
+        c = tg
+        im = cbar_axis.pcolormesh(t, r, c.T,norm= norm, cmap = colmap)
+        cbar_axis.set_theta_zero_location("W")
+        cbar_axis.set_yticklabels([])
+        cbar_axis.set_xticklabels([])
+        cbar_axis.spines['polar'].set_visible(False)
+
+    elif cbar == 'ecc':
+    
+        # Ecc color bar
+        colorbar_location = [0.5, 0.07, 0.8, 0.2]
+        n = 200
+        cbar_axis = vertex_rgb_fig.add_axes(colorbar_location, projection='polar')
+
+        t = np.linspace(0,2*np.pi, n)
+        r = np.linspace(0,1, n)
+        rg, tg = np.meshgrid(r,t)
+        c = tg
+        
+        im = cbar_axis.pcolormesh(t, r, c, norm = mpl.colors.Normalize(0, 2*np.pi), cmap = colmap)
+        cbar_axis.tick_params(pad = 1,labelsize = 15)
+        cbar_axis.spines['polar'].set_visible(False)
+        
+        # superimpose new axis for dva labeling
+        box = cbar_axis.get_position()
+        cbar_axis.set_yticklabels([])
+        cbar_axis.set_xticklabels([])
+        axl = vertex_rgb_fig.add_axes(  [1.8*box.xmin,
+                                    0.5*(box.ymin+box.ymax),
+                                    box.width/600,
+                                    box.height*0.5],
+                                    axisbg = None)
+        axl.spines['top'].set_visible(False)
+        axl.spines['right'].set_visible(False)
+        axl.spines['bottom'].set_visible(False)
+        axl.yaxis.set_ticks_position('right')
+        axl.xaxis.set_ticks_position('none')
+        axl.set_xticklabels([])
+        axl.set_yticklabels([0,3,6], size = 'x-large')
+        axl.set_ylabel('$dva$\t\t', rotation = 0, size = 'x-large')
+        axl.yaxis.set_label_coords(box.xmax+30,0.4)
+        axl.patch.set_alpha(0.5)
+
+    elif cbar == 'discrete':
+
+        # Discrete color bars
+        # -------------------
+        colorbar_location= [0.9, 0.05, 0.03, 0.25]
+        cmaplist = [colmap(i) for i in range(colmap.N)]
+
+        # define the bins and normalize
+        bounds = np.linspace(vmin, vmax, cmap_steps + 1)
+        bounds_label = np.linspace(vmin, vmax, 3)
+        norm = mpl.colors.BoundaryNorm(bounds, colmap.N)
+        
+        cbar_axis = vertex_rgb_fig.add_axes(colorbar_location)
+        cb = mpl.colorbar.ColorbarBase(cbar_axis,cmap = colmap,norm = norm,ticks = bounds_label,boundaries = bounds)
+
+    # add to overalt
+    if add_roi == True:
+        cortex.utils.add_roi(   data = vertex_rgb,
+                                name = roi_name,
+                                open_inkscape = False,
+                                add_path = False,
+                                depth = depth,
+                                thick = thick,
+                                sampler = sampler,
+                                with_curvature = with_curvature,
+                                with_colorbar = with_colorbar,
+                                with_borders = with_borders,
+                                curvature_brightness = curv_brightness,
+                                curvature_contrast = curv_contrast)
+    
+    return vertex_rgb
+    
